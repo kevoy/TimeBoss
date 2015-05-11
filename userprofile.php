588db82737c555126e83dc53860fc07c540d70e1
kevoy <andrellethompson@yahoo.com>
2015-05-10 23:24:12 -0700

    userprofile template

---
+++ userprofile.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Time Boss</title>
+	<link rel="stylesheet" href="stylesheets/bootstrap/css/bootstrap.min.css" type="text/css" >
+	<script src="stylesheets/bootstrap/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="stylesheets/main.css" type="text/css" >
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine:bold,bolditalic|Inconsolata:italic|Droid+Sans|Audiowide|Play">
+
+</head>
+<body>
+	<?php include 'header-template.php' ?>
+	
+	<div id="c_main_container" class="row margin-0">
+
+		<!--put your content here -->
+		
+	</div>
+</body>
+
+
+</html>
