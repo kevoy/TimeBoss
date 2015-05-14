12ff6b11775ede5399f5fa326e40e1800dbdcb0a
kevoy <andrellethompson@yahoo.com>
2015-05-14 07:02:30 -0700

    Added user panel ui

--- account.php
+++ account.php
@@ -13,8 +13,146 @@
 
 
 	
-	<div id="c_main_container" class="row margin-0">
-		
+	<div id="profile_main_container" class="row margin-0">
+		<div class='col-md-2' id="left-bar">
+			<div class="col-md-12" id="actions-nav"></div>
+			<div class="col-md-12 c_tab" id="overview-tab">
+				<h3><span class="margin-10 glyphicon glyphicon-tasks"></span> Overview</h3>
+			</div>
+			<div class="col-md-12 c_tab" id="tasks-tab">
+				<h3><span class="margin-10 glyphicon glyphicon-th-list"></span> Tasks</h3>
+			</div>
+			<div class="col-md-12 c_tab" id="activities-tab">
+				<h3><span class="margin-10 glyphicon glyphicon-headphones"></span> Activities</h3>
+			</div>
+			<div class="col-md-12 c_tab" id="meetings-tab">
+				<h3><span class="margin-10 glyphicon glyphicon-comment"></span> Meetings</h3>
+			</div>
+			<div class="col-md-12 c_tab" id="settings-tab">
+				<h3><span class="margin-10 glyphicon glyphicon-cog"></span> Settings</h3>
+			</div>
+		</div>
+		<div class='col-md-10' id="right-bar">
+			<div class='col-md-12' id="second-nav">
+				<div class="col-md-6" id="pane-nav-title-holder">
+					<h3 id="panel-title">HOME <span class="glyphicon glyphicon-hand-right margin-10-10"></span> <span id="panel-selected">OVERVIEW</span></h3>
+				</div>
+				<div class="col-md-6" id="pane-nav-buttons-holder">
+					<button type="button" class="btn btn-primary"><span class="margin-10 glyphicon glyphicon-ok-circle"></span>Add New Task</button>
+					<button type="button" class="btn btn-info"><span class="margin-10 glyphicon glyphicon-ok-sign"></span>Add New Activity</button>
+					<button type="button" class="btn btn-warning"><span class="margin-10 glyphicon glyphicon-check"></span>Add New Meeting</button>
+				</div>
+			</div>
+			<div id="main-panel" class="col-md-12">
+				<div id="new-task-panel" class="col-md-12">
+					<h3 class="c_subtitle">Add New Task</h3>
+					<div>
+						<form action="" class="form-horizontal">
+							<div class="form-group">
+								<label for="email-field" class="col-sm-2">Task Name</label>
+								<div class="col-sm-7">
+									<input type="email" id="email-field" class="form-control" placeholder="Enter Your Email Address" >
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email-field" class="col-sm-2">Task Location</label>
+								<div class="col-sm-7">
+									<input type="text" id="email-field" class="form-control" placeholder="Enter Location" >
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email-field" class="col-sm-2">Start Date</label>
+								<div class="col-sm-7">
+									<input type="date" id="email-field" class="form-control" placeholder="Enter Your Email Address" >
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email-field" class="col-sm-2">End Date</label>
+								<div class="col-sm-7">
+									<input type="date" id="email-field" class="form-control" placeholder="Enter Your Email Address" >
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email-field" class="col-sm-2">Does this task have a fixed time slot?</label>
+								<div class="col-sm-7">
+									<div class="radio">
+										<label>
+											<input type="radio" id="email-field" name="mandatory" placeholder="Enter Location">Yes
+										</label>
+										<label>
+											<input type="radio" id="email-field" name="mandatory" placeholder="Enter Location" checked>No
+										</label>
+									</div>
+									
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email-field" class="col-sm-2">Start Time</label>
+								<div class="col-sm-7">
+									<input type="time" id="email-field" class="form-control" placeholder="Enter Location" >
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email-field" class="col-sm-2">End Time</label>
+								<div class="col-sm-7">
+									<input type="time" id="email-field" class="form-control" placeholder="Enter Location" >
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email-field" class="col-sm-2">Priority</label>
+								<div class="col-sm-7">
+									<div class="radio">
+										<label>
+											<input type="radio" id="email-field" name="priority" placeholder="Enter Location">High
+										</label>
+										<label>
+											<input type="radio" id="email-field" name="priority" placeholder="Enter Location" checked>Medium
+										</label>
+										<label>
+											<input type="radio" id="email-field" name="priority" placeholder="Enter Location">Low
+										</label>
+									</div>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email-field" class="col-sm-2">Is this a repetitive task?</label>
+								<div class="col-sm-7">
+									<div class="radio">
+										<label>
+											<input type="radio" id="email-field" name="repetitive" placeholder="Enter Location">Yes
+										</label>
+										<label>
+											<input type="radio" id="email-field" name="repetitive" placeholder="Enter Location" checked>No
+										</label>
+									</div>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email-field" class="col-sm-2">Description</label>
+								<div class="col-sm-7">
+									<input type="text" id="email-field" class="form-control" placeholder="Enter Location" >
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email-field" class="col-sm-2">Access</label>
+								<div class="col-sm-7">
+									<div class="radio">
+										<label>
+											<input type="radio" id="email-field" name="access" placeholder="Enter Location" checked>Private
+										</label>
+										<label>
+											<input type="radio" id="email-field" name="access" placeholder="Enter Location" checked>Public
+										</label>
+									</div>
+								</div>
+							</div>
+							<input type="submit" class="btn btn-primary btn-lg  col-sm-offset-2" value="Add Task">
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
 	</div>
 </body>
 
--- meeting.php
+++ meeting.php
@@ -1,29 +1,95 @@
 <?php
-
+require 'User.php';
+if(isset($_REQUEST['class'])){
+	if($_REQUEST['class']=="meeting"){
+		//follow the trend, create parameters for 'method' that should call on the functions you completed below and test eachby visiting example url above replacing 'get_user_id' with the appropiate method parameter
+		if($_REQUEST['method']=="get_num_users"){
+			$meeting_id = $_REQUEST['meeting_id'];
+			return getMeetingInvites();
+		}
+		else if($_REQUEST['method']=="get_user_email"){
+			return getUserId();
+		}
+		else if($_REQUEST['method']=="login"){
+			$email = $_REQUEST['email']; //note these parameters must be in url as well when testing
+			$password = $_REQUEST['password'];
+			return login($email, $password);
+		}
+		//etc..
+	}
+}
 
 function getNumUsers($meeting_id){
-
+	$result = queryDB("Select num_users from group_meeting where meeting_id = '$meeting_id'");
+	$row = mysql_fetch_array($result);
+	echo $row['num_users'];
+	return $row['num_users'];
 }
 function getRequestsAccepted($meeting_id){
-
+	$result = queryDB("Select requests_accepted from group_meeting where meeting_id = '$meeting_id'");
+	$row = mysql_fetch_array($result);
+	echo $row['requests_accepted'];
+	return $row['requests_accepted'];
 }
 function setNumUsers($meeting_id, $num){
-
+	$result = queryDB("Update group_meeting set num_users = '$num' where meeting_id = '$meeting_id'");
+	return $row['num_users'];
 }
 function setRequestsAccepted($meeting_id,$num){
-
+	$result = queryDB("Update group_meeting set requests_accepted = '$num' where meeting_id = '$meeting_id'");
+	return $row['requests_accepted'];
 }
 function getUsersInMeeting($meeting_id){
+	$result = queryDB("Select user_id from groups where meeting_id = '$meeting_id'");
+	$users = array();
+	while($row = mysql_fetch_array($result)){
+		$user[] = $row['user_id'];
+	}
+	echo $user[0];
+	return $users;
+}
+function getMyMeetings(){
+	$cur_user = getUserId();
+	$result = queryDB("Select meeting_id from group_meeting where meeting_leader = '$cur_user'");
+	$meetings = array();
+	while($row = mysql_fetch_array($result)){
+		$meetings[] = $row['meeting_id'];
+	}
+	echo $meetings[0];
+	return $meetings;
 
 }
+function getMeetingOwner($meeting_id){
+	$result = queryDB("Select meeting_leader from group_meeting where meeting_id = '$meeting_id'");
+	$row = mysql_fetch_array($result);
+	echo $row['meeting_leader'];
+	return $row['meeting_leader'];
+}
+function getMeetingInvites(){
+	$cur_user = getUserId();
+	$result = queryDB("Select meeting_id from groups where user_id = '$cur_user'");
+	$meetings = array();
+	while($row = mysql_fetch_array($result)){
+		$meetings[] = $row['meeting_id'];
+	}
+	echo $meetings[0];
+	return $meetings;
+}
 function getRequestStatus($meeting_id, $user){
+
+}
+function getMyMissedInvites(){
+
+}
+function getUsersDeclined($meeting_id){
 
 }
 function getSuggestedTime($meeting_id){
 
 }
-function setSuggestedTime($meeting_id){
-
+function setSuggestedTime($meeting_id, $time){
+	$result = queryDB("Update group_meeting set suggested_time = '$time' where meeting_id = '$meeting_id'");
+	return $row['suggested_time'];
 }
 function createNewMeetingTask($name, $location, $start, $end, $description, $access, $users, $suggested_time){
 
